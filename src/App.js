0445c256b3592aee346eb86efdf41e83a4f3d06a
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-02-29 13:40:08 -0500

    update styles and title logo

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { useContext } from 'react';
 
 import Auth from './components/Auth/Auth';
 import Stories from './components/Stories/Stories';
-import { AuthContext } from './context/auth-context'
-import './App.css'
+import { AuthContext } from './context/auth-context';
+import Logo from './assets/img/timbreallaLogo.png';
+import './App.css';
 
 function App() {
   const authContext = useContext(AuthContext);
 
+
+
   return  (
     <div>
       <header>
-        <h1>Timbrealla!</h1>
+        <div>
+          <span>
+            <div onClick={{/* return to landing page handler */}}>
+              <img src={Logo} alt="timbrealla logo"/>
+            </div>
+          </span>
+        </div>
       </header>
       {authContext.authStatus ? <Stories /> : <Auth />}
     </div>
